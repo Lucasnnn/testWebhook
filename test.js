f8bd527a29ff58da4adb1472713fb6fc7381c73c
Lucas de Oliveira Santos <106331145+Lucasnnn@users.noreply.github.com>
2023-10-17 03:22:28 -0300

    Update test.js

--- test.js
+++ test.js
@@ -10,7 +10,7 @@ app.post("/webhook", async (req, res) => {
   try {
     const data = req.body;
 
-    console.log(data);
+    console.log(JSON.stringfy(data));
 
     // const apiUrl = "http://zap.plug.farm/webhook/bip/status";
 
